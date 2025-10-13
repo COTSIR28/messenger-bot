9cd6fa1c652a29ce2e1f4d5cdefc1dc258e40cc5
COTSIR28 <abel04pascual@gmail.com>
2025-10-13 13:50:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const bodyParser = require("body-parser");
 const request = require("request");
 const app = express();
 
-const PAGE_ACCESS_TOKEN = "YOUR_PAGE_ACCESS_TOKEN";
-const VERIFY_TOKEN = "YOUR_VERIFY_TOKEN";
+const PAGE_ACCESS_TOKEN = "EAFY1TDcrAUsBPupllORIDjgZAvEFhZBdkBgVZB1oJg9o61ZAgNZBeiP4ktnH7YdqZCRQM7iMjnqUPctAzIGT2R0ZBL1oeDGd4Lh8mnQ6VZAqcYCy4Iedtsf5ZAhZAZBqZAL7gkZBWoZBVe1x2fMdhJWZAkhXeCm76K6DD7zmNFjzw2TyRk3aG6MggN1ZCDkYUMVCafa8SzWsXwMpNQZDZD";
+const VERIFY_TOKEN = "MYBOT";
 
 app.use(bodyParser.json());
 
