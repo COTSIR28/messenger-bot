d99647913321663489fd79ad118b0c0810f55a73
COTSIR28 <abel04pascual@gmail.com>
2025-10-16 13:47:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const request = require("request");
 
-const PAGE_ACCESS_TOKEN = "YOUR_PAGE_ACCESS_TOKEN";
+const PAGE_ACCESS_TOKEN = "EAFY1TDcrAUsBPupllORIDjgZAvEFhZBdkBgVZB1oJg9o61ZAgNZBeiP4ktnH7YdqZCRQM7iMjnqUPctAzIGT2R0ZBL1oeDGd4Lh8mnQ6VZAqcYCy4Iedtsf5ZAhZAZBqZAL7gkZBWoZBVe1x2fMdhJWZAkhXeCm76K6DD7zmNFjzw2TyRk3aG6MggN1ZCDkYUMVCafa8SzWsXwMpNQZDZD";
 
 request({
   uri: "https://graph.facebook.com/v17.0/me/messenger_profile",
